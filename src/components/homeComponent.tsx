e60bf6c7c553dfb49a8a10cd1a5dac614c023c86
JUBY VARUGHESE <juby@bcinnovationlabs.com>
2021-04-12 14:56:25 -0400

    Changed styles on input container

--- src/components/homeComponent.tsx
+++ src/components/homeComponent.tsx
@@ -17,11 +17,13 @@ return(
     <div className="header">
         <h1>Add your tasks to the bucket!</h1>  
         <input type="text" placeholder="What needs to be done?" onChange={onBucketItemChange} value={bucketItem} onKeyPress={event => onKeyPressHandler(event)} /> 
+        <div className="container" >
         <ul className="list-items">
             {task.description.map((item) => {
                 return <li key={item}>{item}</li>
             })}
         </ul>
+        </div>
     </div>
     
 );
